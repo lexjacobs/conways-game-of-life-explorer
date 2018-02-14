847326b2d4f9e01269c9032d37651961172439e7
Alex Jacobs <lex.jacobs@gmail.com>
2018-02-14 15:42:16 -0800

    Implement cell surrounding living count

--- src/boardMethods.js
+++ src/boardMethods.js
@@ -1,6 +1,27 @@
 import { initializeArray, randomizeArray, wraparound } from './arrayMethods';
 
 /*
+ returns the number of living (1) cells surrounding cell at index
+ inputs:
+  board: flat array board
+  width: columns
+  index: current cell of interest
+ */
+export function cellCount(board, width, index) {
+  var total = 0;
+
+  // for each of the living surrounding cells, increment total
+  [(index - width - 1), (index - width), (index - width + 1), (index - 1), (index + 1), (index + width - 1), (index + width), (index + width + 1)].forEach(position => {
+    var living = board[wraparound(index, (position - index), board.length)];
+    if(living) {
+      total++;
+    }
+  });
+
+  return total;
+}
+
+/*
  create a board representation (just a flat array)
  inputs:
    width in columns
--- src/boardMethods.test.js
+++ src/boardMethods.test.js
@@ -1,11 +1,38 @@
-import { createBoard } from './boardMethods';
+import {
+  cellCount,
+  createBoard
+} from './boardMethods';
+
+test('it correctly counts the number of living cells surrounding a cell, which is 0 if everyone is dead', () => {
+  var board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  expect(board.length).toEqual(9);
+  var result = cellCount(board, 3, 5);
+  expect(result).toBe(0);
+  // this should work regardless of starting index
+  for (var i = 0; i < board.length; i++) {
+    var result = cellCount(board, 3, i);
+    expect(result).toBe(0);
+  }
+});
+
+test('it correctly counts the number of living cells surrounding a cell, which is 8 if everyone is alive', () => {
+  var board = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+  expect(board.length).toEqual(9);
+  var result = cellCount(board, 3, 3, 5);
+  expect(result).toBe(8);
+  // this should work regardless of starting index
+  for (var i = 0; i < board.length; i++) {
+    var result = cellCount(board, 3, i);
+    expect(result).toBe(8);
+  }
+});
 
 test('it creates a board with the expected width, height, and default weighting', () => {
   var result = createBoard(1, 1)
   expect(result).toEqual([0]);
   result = createBoard(2, 1)
   expect(result).toEqual([0, 0]);
-  result = createBoard(3,3)
+  result = createBoard(3, 3)
   expect(result).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0]);
   result = createBoard(20, 10)
   expect(result.length).toEqual(200);
